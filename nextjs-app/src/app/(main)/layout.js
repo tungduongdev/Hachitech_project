8172c3939ebbe448b7d1b29374423fbc5e3c9fe0
tungduongdev <dtc2154802010119@ictu.edu.vn>
2025-03-12 15:10:49 +0700

    test_ci/cd_02

--- nextjs-app/src/app/(main)/layout.js
+++ nextjs-app/src/app/(main)/layout.js
@@ -16,7 +16,7 @@ export default function RootLayout({ children }) {
       <Head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Ecomer website</title>
+        <title>Ecommerse website</title>
       </Head>
       <body>
         <Layout>
